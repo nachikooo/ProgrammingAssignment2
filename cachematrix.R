4f7fd98996cb51a4aebccc750daab1180bc970f7
nachikooo <45351188+nachikooo@users.noreply.github.com>
2020-10-23 18:41:08 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Functions:
+## - makeCacheMatrix - A function to create a special "matrix" object and 
+##                      cache its inverse
+## - cacheSolve -  A function which return an inverse of the 'matrix' object
+##                  if it already exist in the cache and the object remain 
+##                  unchanged. Else, the function will calculate the inverse
+##                  on the special 'matrix' returned by makeCacheMatrix.
 ## Write a short comment describing this function
+
+#Short Comment
+## A function that received a special "matrix" object and cache the inverse
+## of the matrix and store in the global environment
+## It does nothing except set and get the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
  inv <- NULL
@@ -12,11 +24,21 @@ makeCacheMatrix <- function(x = matrix()) {
       get <- function() {x}
       setInverse <- function(inverse) {inv <<- inverse}
       getInverse <- function() {inv}
-      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)  
 }
 
-cacheSolve <- function(x, ...){
-      inv <- x$getInverse()
+
+## Write a short comment describing this function
+## A function that will
+## > Check if the inverse object exist and the matrix is unchanged by getting
+##    the stored inverse matrix calling from makeCacheMatrix
+## > Return the inverse object in cache if it satisfy the first rules.
+## > Else, compute the inverse of the special 'matrix', store in 
+##    makeCacheMatrix for future use and return the new inverse object.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+     inv <- x$getInverse()
       if(!is.null(inv)){
             message("getting cached data")
             return(inv)
@@ -25,11 +47,4 @@ cacheSolve <- function(x, ...){
       inv <- solve(mat, ...)
       x$setInverse(inv)
       inv
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 }
